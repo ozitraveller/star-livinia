04cc1503555bba270ee3f2e3dfaffd63f80bfad2
ozitraveller <ozitraveller@gmail.com>
2016-04-10 11:59:40 +1000

    8.4 - dmenu

--- jwm-64/config/includes.chroot/etc/skel/bin/run.sh
+++ jwm-64/config/includes.chroot/etc/skel/bin/run.sh
@@ -16,8 +16,7 @@ sleep 1
 	compton --config ~/.compton.conf &
 	
 	(sleep 2 && wicd-gtk --tray) &
-#	(sleep 2 && xbattbar-acpi -s 2 -p left -I "dark green" -O "black") &
-#	(sleep 3 && volumeicon --tray) &
+	(sleep 3 && volumeicon --tray) &
 	(conky --pause=3 -c ~/.conkyrc) &
 	(sleep 3 && clipit) &
 	nitrogen --restore &
